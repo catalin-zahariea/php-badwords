e01e22c8db19084b4156e9774afcefd1fae55783
CATALIN ZAHARIEA <zahariea.c@gmail.com>
2021-05-18 10:04:17 +0200

    Updated comments.

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+    // Filter input by query
+    $filter = $_GET['filter'];
+
+    // Default paragraph
+    $myString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    
+    // Filtering default paragraph 
+    $filteredString = str_replace( $filter, "***", $myString );
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,25 +24,14 @@
 
     <main>
         
-        <?php
-        // Filter input by query
-        $filter = $_GET['filter'];
-
-        // Default paragraph
-        $myString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-        
-        // Filtering default paragraph 
-        $filteredString = str_replace( $filter, "***", $myString );
-        ?>
-        
         <h1>What is Lorem Ipsum</h1>
 
-        // Printing filtered paragraph.
+        <!-- Printing filtered paragraph. -->
         <p>
             <?php echo $filteredString; ?>
         </p>
 
-        // Printing paragraph length.
+        <!-- Printing paragraph length. -->
         <p>
             <?php echo "Paragraph length: " . strlen($filteredString) ?>
         </p>
